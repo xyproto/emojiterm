11b7c274ae5189f198a68637c243532bf2692bc8
Alexander F. Rødseth <rodseth@gmail.com>
2020-03-09 11:39:28 +0100

    Initial commit of source code

---
+++ main.go
@@ -0,0 +1,178 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"os"
+	"regexp"
+	"runtime"
+	"strconv"
+	"strings"
+
+	"github.com/eliukblau/pixterm/pkg/ansimage"
+	"github.com/google/go-github/v29/github"
+	"github.com/lucasb-eyer/go-colorful"
+	"golang.org/x/crypto/ssh/terminal"
+)
+
+const (
+	versionString      = "0.1.0"
+	flagRows      uint = 16 // height
+	flagCols      uint = 32 // width
+)
+
+// Fetch a map of all available emojis on GitHub,
+// using the GitHub API.
+// * The keys are names, like "snowman".
+// * The values are URLs to PNG images.
+func fetchEmojis() (map[string]string, error) {
+	client := github.NewClient(nil)
+	m, _, err := client.ListEmojis(context.Background())
+	if err != nil {
+		return nil, err
+	}
+	return m, err
+}
+
+// This function is based on code from github.com/eliukblau/pixterm,
+// which is also licensed under MPL2.
+func display(url, description string) error {
+	const (
+		flagDither uint   = 0         // dither mode, 0, 1 or 2
+		flagNoBg   bool   = false     // disable background color?
+		flagScale  uint   = 0         // scale method 0, 1 or 2
+		matteColor string = "#000000" // matte color
+	)
+	var (
+		pix        *ansimage.ANSImage
+		err        error
+		isTerminal = terminal.IsTerminal(int(os.Stdout.Fd()))
+		tx         = 80
+		ty         = 24
+	)
+
+	// get terminal size
+	if isTerminal {
+		tx, ty, err = terminal.GetSize(int(os.Stdout.Fd()))
+		if err != nil {
+			return err
+		}
+	}
+
+	// use custom terminal size (if applies)
+	if ty--; flagRows != 0 { // no custom rows? subtract 1 for prompt spacing
+		ty = int(flagRows) + 1 // weird, but in this case is necessary to add 1 :O
+	}
+	if flagCols != 0 {
+		tx = int(flagCols)
+	}
+
+	// get scale mode from flag
+	sm := ansimage.ScaleMode(flagScale)
+
+	// get dithering mode from flag
+	dm := ansimage.DitheringMode(flagDither)
+
+	// set image scale factor for ANSIPixel grid
+	sfy, sfx := ansimage.BlockSizeY, ansimage.BlockSizeX // 8x4 --> with dithering
+	if ansimage.DitheringMode(flagDither) == ansimage.NoDithering {
+		sfy, sfx = 2, 1 // 2x1 --> without dithering
+	}
+
+	mc, err := colorful.Hex(matteColor) // RGB color from Hex format
+	if err != nil {
+		return fmt.Errorf("matte color : %s is not a hex-color: %s", matteColor, err.Error())
+	}
+
+	// create new ANSImage from url
+	if matched, _ := regexp.MatchString(`^https?://`, url); matched {
+		pix, err = ansimage.NewScaledFromURL(url, sfy*ty, sfx*tx, mc, sm, dm)
+	} else {
+		pix, err = ansimage.NewScaledFromFile(url, sfy*ty, sfx*tx, mc, sm, dm)
+	}
+	if err != nil {
+		return err
+	}
+
+	// draw ANSImage to terminal
+	if isTerminal {
+		ansimage.ClearTerminal()
+	}
+
+	pix.SetMaxProcs(runtime.NumCPU()) // maximum number of parallel goroutines!
+
+	pix.DrawExt(false, flagNoBg)
+
+	if !isTerminal {
+		return errors.New("not a terminal")
+	}
+
+	linesUp := strconv.Itoa(int(flagRows)/2 + 2)
+	colsRight := strconv.Itoa(int(flagCols) + 5)
+
+	fmt.Println()
+	fmt.Print("\033[s") // save cursor position
+
+	fmt.Print("\033[" + linesUp + "A")   // move N lines up
+	fmt.Print("\033[" + colsRight + "C") // move 30 to the right
+	fmt.Print(description)               // print the description
+
+	// restore cursor position
+	fmt.Print("\033[u")
+
+	fmt.Println()
+
+	return nil
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, "Usage: "+os.Args[0]+" [-l] [searchword]")
+		os.Exit(1)
+	}
+	searchword := os.Args[1]
+
+	emojis, err := fetchEmojis()
+	if err != nil {
+		panic(err)
+	}
+
+	if searchword == "-l" {
+		// List all names
+		for name := range emojis {
+			fmt.Println(":" + name + ":")
+		}
+		return
+	}
+
+	found := false
+
+	// Does one of the emoji names start with this string?
+	for name, url := range emojis {
+		if strings.HasPrefix(name, searchword) {
+			display(url, ":"+name+":")
+			found = true
+			break
+		}
+	}
+
+	// Does one of the emoji names contain this string?
+	if !found {
+		for name, url := range emojis {
+			if strings.Contains(name, searchword) {
+				if err := display(url, ":"+name+":"); err != nil {
+					fmt.Fprintln(os.Stderr, err.Error())
+					os.Exit(1)
+				}
+				found = true
+				break
+			}
+		}
+	}
+
+	if !found {
+		fmt.Fprintln(os.Stderr, "Not found: "+searchword)
+		os.Exit(1)
+	}
+}
